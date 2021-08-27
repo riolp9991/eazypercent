2bca6ad835b4789c9b8e03f600a80543130ed81b
riolp9991 <riolp9991@gmail.com>
2021-08-27 14:28:58 -0400

    icons and others

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Asistente de calculos porcentuales'),
+      home: MyHomePage(title: 'Eazypercent'),
     );
   }
 }
